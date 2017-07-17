1a05b7edc909f1b5d42c13d04ada16020a3de48d
lainenR <revay1@hotmail.com>
2017-07-17 15:05:40 +0200

    Changed for summertime

--- MyMoridgeServer/Controllers/BookingController.cs
+++ MyMoridgeServer/Controllers/BookingController.cs
@@ -40,8 +40,8 @@ namespace MyMoridgeServer.Controllers
                 {
                     BookingEvent bookingEvent = new BookingEvent();
 
-                    bookingEvent.StartDateTime = Convert.ToDateTime(bookingEventDTO.StartDateTime).AddHours(6); //Compensate for timedifference between client- and serviceserver
-                    bookingEvent.EndDateTime = Convert.ToDateTime(bookingEventDTO.EndDateTime).AddHours(6); //Compensate for timedifference between client- and serviceserver;
+                    bookingEvent.StartDateTime = Convert.ToDateTime(bookingEventDTO.StartDateTime).AddHours(7); //Compensate for timedifference between client- and serviceserver
+                    bookingEvent.EndDateTime = Convert.ToDateTime(bookingEventDTO.EndDateTime).AddHours(7); //Compensate for timedifference between client- and serviceserver;
                     bookingEvent.CustomerOrgNo = bookingEventDTO.CustomerOrgNo;
                     bookingEvent.CustomerEmail = bookingEventDTO.CustomerEmail;
                     bookingEvent.CustomerAddress = bookingEventDTO.CustomerAddress;
